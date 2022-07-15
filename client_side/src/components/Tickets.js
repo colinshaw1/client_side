68abed8ad26a8f56ac0e9d70d4d777386721f761
Colin Shaw <colin.shaw400@outlook.com>
2022-07-15 21:29:01 +0000

    Add routes to tickets.js page for all displays

--- client_side/src/components/Tickets.js
+++ client_side/src/components/Tickets.js
@@ -1,7 +1,18 @@
+//import component pages into ticket page
+import CustomerCards from "./CustomerCards"
+import DeleteSection from "./DeleteSection"
+import PriorityDisplay from "./PriorityDisplay"
+import ProgressDislay from "./ProgressDisplay"
+import StatusDisplay from "./StatusDisplay"
+import UserImages from "./UserImage"
+//import 
+import {Link} from 'react-router-dom'
+
 const Tickets = () => {
     return (
         <div className="ticket-card">
-            
+            <div className="ticket-color"></div>
+
         </div>
     )
 }
