9d6f32c647248959c5f9bb26dd5228d0376396b8
Colin Shaw <colin.shaw400@outlook.com>
2022-07-16 09:50:50 +0000

    add test data to dashboard and console log unique categoiries to test

--- client_side/src/components/Tickets.js
+++ client_side/src/components/Tickets.js
@@ -5,14 +5,22 @@ import PriorityDisplay from "./PriorityDisplay"
 import ProgressDislay from "./ProgressDisplay"
 import StatusDisplay from "./StatusDisplay"
 import UserImages from "./UserImage"
-//import 
+//import link so it can be used from react router dom. 
 import {Link} from 'react-router-dom'
 
 const Tickets = () => {
     return (
         <div className="ticket-card">
             <div className="ticket-color"></div>
-
+            <link>
+            <h3>title</h3>
+            <UserImages/>
+            <CustomerCards/>
+            <DeleteSection/>
+            <PriorityDisplay/>
+            <ProgressDislay/>
+            <StatusDisplay/>
+            </link>
         </div>
     )
 }
--- client_side/src/pages/Dashboards.js
+++ client_side/src/pages/Dashboards.js
@@ -2,6 +2,38 @@
 import Tickets from "../components/Tickets"
 
 const Dashboard = () => {
+
+    const tickets = [
+        {
+            category: 'Q1 2022',
+            color: 'blue',
+            title: 'sales',
+            owener: 'Colin',
+            user: 'Colin',
+            status: 'done',
+            priority: 6,
+            progress: '40',
+            description: 'fake data to test',
+            timestampe: '16-07-2022 10:42'
+        },
+        {
+            category: 'Q2 2022',
+            color: 'green',
+            title: 'sales',
+            owener: 'Colin s',
+            user: 'Colin s',
+            status: 'in progress',
+            priority: 4,
+            progress: '60',
+            description: 'fake data to test',
+            timestampe: '16-07-2022 10:42'
+        },
+
+    ]
+    const uniqueCategories = [
+        ...new Set(tickets?.map(({category}) => category))
+    ]
+    console.log(uniqueCategories)
     return (
         //styling dashboard
         <div>Dashboard
