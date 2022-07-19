d10bb74fa9da9e5a66682dbbdd6b13cb547ce13e
Colin Shaw <colin.shaw400@outlook.com>
2022-07-19 20:37:06 +0000

    Add fix styling for navbar height and font size

--- client_side/src/components/Navbar.js
+++ client_side/src/components/Navbar.js
@@ -3,19 +3,19 @@ import { useNavigate } from 'react-router-dom'
 
 const Nav = () => {
 
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  return (
-    <nav>
-      <div className="logo-container" onClick={() => navigate('/')}>
-        ClientSide
-      </div>
-      <div className="controls-container">
-        <div className="icon" onClick={() => navigate('/ticket')}>➕</div>
-        <div className="icon" onClick={() => navigate('/')}>❮❮</div>
-      </div>
-    </nav>
-  )
+    return (
+        <nav>
+            <div className="logo-container" onClick={() => navigate('/')}>
+                ClientSide
+            </div>
+            <div className="controls-container">
+                <div className="icon" onClick={() => navigate('/ticket')}>➕</div>
+                <div className="icon" onClick={() => navigate('/')}>❮❮</div>
+            </div>
+        </nav>
+    )
 }
 
 export default Nav
