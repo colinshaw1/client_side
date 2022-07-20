cbb9e5e6d1e7df04b2e54b03b905660bdec129aa
Colin Shaw <colin.shaw400@outlook.com>
2022-07-20 19:04:50 +0000

    Add edit mode and event handlers fucntions to handle submitting data and where the data will state in its intial state

--- client_side/src/pages/CustomerTickets.js
+++ client_side/src/pages/CustomerTickets.js
@@ -1,6 +1,39 @@
+import { useState } from "react"
+
 const CustomerTickets = () => {
+    //setting the from data in its intal state
+    const[fromData, setFromData] = useState({
+        status:'not started',
+        progress: 0
+    })
+    const editMode = false
+
+    const handleSubmit = () => {
+        console.log('submitted')
+    }
+    const handleChange = () => {
+        console.log('changed')
+    }
     return (
-        <div></div>
+        <div className="tickets">
+            <h1>{editMode ? 'Hey there would you like to update your ticket?' : 'Please, select the ➕ icon to create a new ticket'}</h1>
+            <div className="ticket-container">
+                <form onSubmit={handleSubmit}>
+                    <section>
+                        <label htmlFor="title">Title</label>
+                        <input
+                            id="title"
+                            name="title"
+                            type="text"
+                            onChnage={handleChange}
+                            required={true}
+                            value={fromData.title}
+                        />
+                    </section>
+                </form>
+
+            </div>
+        </div>
     )
 }
 
