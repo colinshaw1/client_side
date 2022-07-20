574f11a1359e213bef7255ec9465ec77405198d0
Colin Shaw <colin.shaw400@outlook.com>
2022-07-20 20:25:24 +0000

    add categories fields for title, description and categories

--- client_side/src/pages/CustomerTickets.js
+++ client_side/src/pages/CustomerTickets.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 const CustomerTickets = () => {
     //setting the from data in its intal state
-    const [fromData, setFromData] = useState({
+    const [formData, setFormData] = useState({
         status: 'not started',
         progress: 0
     })
@@ -17,12 +17,14 @@ const CustomerTickets = () => {
         const value = e.target.value
         const name = e.target.name
         //pass through previous state and add name and value
-        setFromData((prevState) => ({
+        setFormData((prevState) => ({
             ...prevState,
-            [name] : value
+            [name]: value
         })
         )
     }
+
+    const categories = ['test1', 'test2']
     return (
         <div className="tickets">
             <h1>{editMode ? 'Hey there would you like to update your ticket?' : 'Please, select the ➕ icon to create a new ticket'}</h1>
@@ -36,7 +38,37 @@ const CustomerTickets = () => {
                             type="text"
                             onChnage={handleChange}
                             required={true}
-                            value={fromData.title}
+                            value={formData.title}
+                        />
+                        <label htmlFor="description">Description</label>
+                        <input
+                            id="description"
+                            name="description"
+                            type="text"
+                            onChnage={handleChange}
+                            required={true}
+                            value={formData.description}
+                        />
+                        <label>Category</label>
+                        <select
+                            name="category"
+                            value={formData.category}
+                            onChnage={handleChange}
+                        >
+                            {categories?.map((category, _index) => (
+                                <option key={_index} value={category}>{category}</option>
+                            ))}
+                            
+                        </select>
+
+                        <label htmlFor="category">Update Category</label>
+                        <input
+                            id="category"
+                            name="category"
+                            type="text"
+                            onChnage={handleChange}
+                            required={true}
+                            value={formData.category}
                         />
                     </section>
                 </form>
